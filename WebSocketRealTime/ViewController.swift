66a8e1ce965f8b72b5b69a87e20a9049449c01b3
Alan Casas <alan_casas_arevalo@carrefour.com>
2021-11-13 10:51:12 +0100

    Web socket was implemented

--- WebSocketRealTime/ViewController.swift
+++ WebSocketRealTime/ViewController.swift
@@ -10,9 +10,18 @@ protocol WebSockets {
 class ViewController: UIViewController {
 
     private var webSocket: URLSessionWebSocketTask?
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Close", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
 
         let urlSession = URLSession(
                 configuration: .default,
@@ -20,28 +29,68 @@ class ViewController: UIViewController {
                 delegateQueue: OperationQueue()
         )
 
+        // The url test online is this one
+        // https://www.piesocket.com/websocket-tester
         let url = URL(string: "wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self")!
         webSocket = urlSession.webSocketTask(with: url)
         webSocket?.resume()
+        closeButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        view.addSubview(closeButton)
+        closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
+        closeButton.center = view.center
     }
 }
 
-extension ViewController : WebSockets {
+extension ViewController: WebSockets {
     func ping() {
+        webSocket?.sendPing { error in
+            if let error = error {
+                print("Error en ping\(error.localizedDescription)")
+            }
+        }
     }
 
-    func close() {
+    @objc func close() {
+        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
     }
 
     func send() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+            self.send()
+            self.webSocket?.send(.string("Enviar el siguiente mensaje \(Int.random(in: 0...100))")) { error in
+                if let error = error {
+                    print("Error en send\(error.localizedDescription)")
+                }
+            }
+        }
     }
 
     func receive() {
+        webSocket?.receive { [weak self] result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    print("Got data: \(data)")
+                case .string(let message):
+                    print("Got message: \(message)")
+                @unknown default:
+                    break
+                }
+            case .failure(let error):
+                print("Error en send\(error.localizedDescription)")
+            }
+
+            self?.receive()
+        }
     }
 }
 
-extension ViewController : URLSessionWebSocketDelegate {
+extension ViewController: URLSessionWebSocketDelegate {
     public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        ping()
+        send()
+        receive()
         print("Did open connection")
     }
 
